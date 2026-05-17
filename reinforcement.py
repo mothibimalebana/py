d6954254d887b7b0d8313deedc52158fb3b0c441
unknown <mothibi.mm12@gmail.com>
2026-05-17 22:36:11 +0200

    R-1.1

---
+++ reinforcement.py
@@ -0,0 +1,7 @@
+"""
+R-1.1 Write a short Python function, is_multiple(n, m), that takes two integer
+values and returns True if n is a multiple of m, that is,n = mi for some
+integer i,andFalse otherwise.
+"""
+def is_multiple(n, m):
+    return True if m % n == 0 else False
