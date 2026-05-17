dd37a9546584449282aaab7250dbfa71385de20e
unknown <mothibi.mm12@gmail.com>
2026-05-17 23:01:56 +0200

    R-1.3

--- reinforcement.py
+++ reinforcement.py
@@ -14,3 +14,22 @@ cannot use the multiplication, modulo, or division operators.
 """
 def is_even(k):
     return True if k in range(0, k+1, 2) else False
+
+
+"""
+R-1.3 Write a short Python function, minmax(data), that takes a sequence of
+one or more numbers, and returns the smallest and largest numbers, in the
+form of a tuple of length two. Do not use the built-in functions min or
+max in implementing your solution.
+"""
+def minmax(data):
+    u = data[0] # max variable
+    l = data[0] # min variable
+
+    for i in range(len(data)):
+        print(data[i], i)
+        if data[i] > u:
+            u = data[i]
+        elif data[i] < l:
+            l = data[i]
+    return (l, u)
