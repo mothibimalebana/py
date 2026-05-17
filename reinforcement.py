cadbcb2b5af716b3e3d4363727b27b16c0a50e2e
unknown <mothibi.mm12@gmail.com>
2026-05-17 22:43:48 +0200

    R-1.2

--- reinforcement.py
+++ reinforcement.py
@@ -5,3 +5,12 @@ integer i,andFalse otherwise.
 """
 def is_multiple(n, m):
     return True if m % n == 0 else False
+
+
+"""
+R-1.2 Write a short Python function, is_even(k), that takes an integer value and
+returns True if k is even, and False otherwise. However, your function
+cannot use the multiplication, modulo, or division operators.
+"""
+def is_even(k):
+    return True if k in range(0, k+1, 2) else False
